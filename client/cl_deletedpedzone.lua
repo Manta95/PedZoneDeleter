444ff13e69c935b1260f47cc20819f1d265ed248
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2024-10-14 08:21:14 +0200

    Create cl_deletedpedzone.lua

---
+++ client/cl_deletedpedzone.lua
@@ -0,0 +1,44 @@
+local areas = {
+    { coords = vector3(-1095.872559, -833.806580, 37.695602), radius = 20.0 }, -- LSPD
+    { coords = vector3(-1200.261597, -895.782166, 19.983397), radius = 10.0 }  -- Burgershot
+}
+
+local playerInArea = {}
+for i = 1, #areas do
+    playerInArea[i] = false
+end
+
+RegisterNetEvent('clearAreaOfPeds')
+AddEventHandler('clearAreaOfPeds', function(areas)
+    for _, area in ipairs(areas) do
+        local coords = area.coords
+        local radius = area.radius
+
+        ClearAreaOfPeds(coords.x, coords.y, coords.z, radius, 1)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+
+        for i, area in ipairs(areas) do
+            local coords = area.coords
+            local radius = area.radius
+
+            if #(playerCoords - coords) <= radius then
+                if not playerInArea[i] then
+                    TriggerServerEvent('clearAreaOfPeds', { area })
+                    playerInArea[i] = true
+                end
+            else
+                if playerInArea[i] then
+                    playerInArea[i] = false
+                end
+            end
+        end
+
+        Wait(1000)
+    end
+end)
